7596a69bcd9d17c402a19cd42f455c4d9919fefb
PC-BARTEK\barto <bartoszbizon97@gmail.com>
2026-08-17 09:22:18 +0200

    Remove obsolete frontend

--- api/obj/Debug/net10.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net10.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+080d88b902d84768bfc60b92e0d9137ad66b8cf6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8fdcccdbec17bcb3bf7ed6ffe51437c6cfb489cb")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
